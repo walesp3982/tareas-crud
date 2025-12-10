07509b35f6ac044064572c2c5fe23fbf756ee6da
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2025-12-09 23:32:56 -0400

    add authentification logic

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Hash;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    //
+    final public function login(LoginRequest $request): JsonResponse
+    {
+        $dataRequest = $request->validated();
+
+        $user = User::where('email', $dataRequest['email'])->first();
+
+        if (!$user || !Hash::check($dataRequest['password'], $user->password)) {
+            return response()->json([
+                "message" => "La credenciales son incorrectas",
+                "errors" => [
+                    "email" => ["Las credenciales son incorrectas"]
+                ]
+            ], 401);
+        }
+
+        return response()->json([
+            'token' => $user->createToken($dataRequest['device_name'])->plainTextToken
+        ]);
+    }
+
+    final public function logout(): JsonResponse
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logged out'
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', action: [AuthController::class, 'login']);
+
+Route::get('/test', function () {
+    return response()->json(["msg" => "Hola"]);
+});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, "logout"]);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
