5f30529bd8fb61788e70d808b2f168248deac810
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2025-12-22 19:40:00 -0400

    [frontend] styling perfomance

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 // import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import { Register } from './pages/Register'
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
