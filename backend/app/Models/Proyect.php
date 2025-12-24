fe57ac4655e563546d3ed0e2972c7feb2c274083
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2025-12-23 20:48:05 -0400

    [backend] add PHPDoc to Proyect Model

--- backend/app/Models/Proyect.php
+++ backend/app/Models/Proyect.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|string  $start_date
+ * @property \Illuminate\Support\Carbon|string  $end_date
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ */
 class Proyect extends Model
 {
     /** @use HasFactory<\Database\Factories\ProyectFactory> */
@@ -15,7 +23,14 @@ class Proyect extends Model
 
     protected $fillable = [
         "user_id",
-        "name"
+        "name",
+        "start_date",
+        "end_date"
+    ];
+
+    protected $casts = [
+        'start_date' => 'date:Y-m-d',
+        'end_date' => 'date:Y-m-d',
     ];
 
     public function user()
