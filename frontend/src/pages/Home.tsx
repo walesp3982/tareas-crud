7a3833c383772e4b8aca22c24933efa68381ae52
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2025-12-24 09:46:41 -0400

    [frontend] Implement Suggest component in page'application

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,15 +2,15 @@ import { Link } from "react-router"
 import { LoginForm } from "../components/forms/LoginForm"
 import { Title } from "../components/Title"
 import { Subtitle } from "../components/Subtitle"
+import { Suggest } from "../components/Suggest"
 export default function Home() {
     return (
         <>
             <Title>Sistema de tareas - CRUD</Title>
             <Subtitle>Iniciar Sesión</Subtitle>
-            <div>
+            <Suggest>
                 ¿No se encuentra registrado? <Link to="/register" >Cree una nueva cuenta</Link>
-
-            </div>
+            </Suggest>
             <LoginForm></LoginForm>
         </>
     )
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,12 +2,15 @@ import type { JSX } from "react";
 import { Link } from "react-router";
 import { Title } from "../components/Title";
 import { Subtitle } from "../components/Subtitle";
+import { Suggest } from "../components/Suggest";
 
 export function Register(): JSX.Element {
     return (<>
         <Title>Sistema de tareas - CRUD</Title>
         <Subtitle>Registro</Subtitle>
-        <Link to="/" >Volver al home</Link>
+        <Suggest>
+            ¿Ya se encuentra registrado? <Link to="/" >Volver al home</Link>
+        </Suggest>
         <p>Bienvenido al registro</p>
     </>)
 }
