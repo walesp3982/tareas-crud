991e219eef5df1c8bb659629c95001b02d40f231
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2025-12-23 20:48:36 -0400

    [backend] Add start_date and end_date column to proyects table

--- backend/database/migrations/2025_12_10_033903_create_proyects_table.php
+++ backend/database/migrations/2025_12_10_033903_create_proyects_table.php
@@ -14,6 +14,8 @@ return new class extends Migration {
             $table->id();
             $table->string("name");
             $table->foreignId("user_id")->constrained();
+            $table->date("start_date");
+            $table->date("end_date");
             $table->softDeletes();
             $table->timestamps();
         });
