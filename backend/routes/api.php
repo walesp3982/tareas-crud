ea2df4cfa0c0a71d682942b75b722b99d1b8acbf
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2025-12-23 20:50:28 -0400

    [backend] add proyect API to auth:sanctum

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProyectController;
 use App\Models\Proyect;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,6 +16,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::apiResource('proyect', ProyectController::class);
 });
 
-Route::apiResource('proyect', Proyect::class);
