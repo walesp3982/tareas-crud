10f492dcf0021267398e3a676b522111be215b99
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2025-12-29 11:25:00 -0400

    [frontend] add disabled props attributes in Button

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -2,12 +2,17 @@ import type React from "react"
 import styles from './Button.module.css'
 interface ButtonProps {
     children: React.ReactNode;
-    onClick?: () => void
+    onClick?: () => void;
+    disabled?: boolean;
 }
 
-export function Button({ children, onClick }: ButtonProps) {
+export function Button({ children, onClick, disabled }: ButtonProps) {
     return (
-        <button className={styles.button} type="submit" onClick={onClick}>
+        <button
+            className={styles.button}
+            type="submit"
+            onClick={onClick}
+            disabled={disabled}>
             {children}
         </button>
     )
