c05c4d437f211a56442e7fa279e9ebd79ba49b27
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-01-26 14:55:17 -0400

    [frontend]  add dashboard Component

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,13 @@
+import type { JSX } from "react";
+import { Subtitle } from "../components/Subtitle";
+
+
+export function Dashboard(): JSX.Element {
+    return (
+        <>
+            <Subtitle>Dashboard</Subtitle>
+
+            <p>Esto es un dashboard</p>
+        </>
+    )
+}
