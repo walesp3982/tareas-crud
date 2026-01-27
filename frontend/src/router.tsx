e63c189d488bf60badabe7bf7c9ef21c559b35fc
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-01-27 10:50:10 -0400

    [frontend] refactoring routes

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,24 +1,73 @@
 import { Login } from './pages/Login'
-import { createBrowserRouter } from 'react-router'
+import { createBrowserRouter, type LoaderFunctionArgs } from 'react-router'
 import { Register } from './pages/Register'
 import { Dashboard } from './pages/Dashboard'
 import { Layout } from './layouts/Guest'
 import { App } from './layouts/App'
+import { redirect } from 'react-router'
+import { getTokenAuth, validateToken } from './services/auth'
+
 
 export const router = createBrowserRouter([
     {
         path: "/",
         Component: Layout,
         children: [
-            { index: true, Component: Login },
-            { path: "register", Component: Register }
+            {
+                index: true,
+                Component: Login,
+                loader: async () => {
+                    await requireGuest()
+                    return null
+                }
+            },
+            {
+                path: "register",
+                Component: Register,
+                loader: async () => {
+                    await requireGuest()
+                    return null
+                }
+            }
+
         ]
     },
     {
-        path: "/app",
+        path: "app",
         Component: App,
+        loader: protectedLoader,
         children: [
-            { index: true, Component: Dashboard }
+            { index: true, Component: Dashboard },
         ]
     }
 ])
+
+async function requireAuth(request: Request) {
+    const token = getTokenAuth();
+
+    if (!token) {
+        const url = new URL(request.url)
+        throw redirect(`/?from=${url.pathname}`)
+    }
+
+    try {
+        await validateToken()
+    } catch {
+        throw redirect("/")
+    }
+}
+
+async function requireGuest() {
+    const token = getTokenAuth();
+
+    if (token) {
+        throw redirect("/app")
+    }
+
+    return null
+}
+async function protectedLoader({ request }: LoaderFunctionArgs) {
+    await requireAuth(request)
+}
+
+
