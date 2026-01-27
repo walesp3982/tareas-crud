2cb40ee064ecb954f823d67a8e977c8e07ca7e41
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-01-27 16:08:41 -0400

    [frontend] add navigation to app layout

--- frontend/src/layouts/App.tsx
+++ frontend/src/layouts/App.tsx
@@ -1,13 +1,22 @@
 import type { JSX } from "react";
 import { Title } from "../components/Title";
 import { NavLink, Outlet } from "react-router";
-
+import { useNavigate } from "react-router-dom";
+import { Auth } from "../services/auth";
 
 export const App = (): JSX.Element => {
+
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        Auth.logout()
+        navigate('/', { replace: true })
+    }
     return <>
         <Title>Sistema de tareas - CRUD</Title>
         <div className="navigation">
-            <NavLink to="/Dashboard">Principal</NavLink>
+            <NavLink to="/app">Principal</NavLink>
+            <a onClick={handleLogout} style={{ cursor: 'pointer' }}>Cerrar sesión</a>
         </div>
 
         <Outlet />
