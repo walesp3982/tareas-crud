9135eefde66720829b0678e35261b0881b1be9fc
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-01-27 16:09:16 -0400

    [frontend] delete register unnesesary hooks

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,24 +1,16 @@
-import { useState, type JSX } from "react";
+import { type JSX } from "react";
 import { Link } from "react-router";
 import { Subtitle } from "../components/Subtitle";
 import { Suggest } from "../components/Suggest";
 import { RegisterForm } from "../components/forms/RegisterForm";
 
 export function Register(): JSX.Element {
-
-    const [registerSend, setRegisterSend] = useState<boolean>(true);
-
-    const registerCorrect = () => { setRegisterSend(true) };
-
-    if (registerSend) {
-        console.log("Registrado")
-    }
     return (
         <>
             <Subtitle>Registro</Subtitle>
             <Suggest>
                 ¿Ya se encuentra registrado? <Link to="/" >Volver al home</Link>
             </Suggest>
-            <RegisterForm onCorrectSubmit={registerCorrect} />
+            <RegisterForm />
         </>)
 }
