105b1449cc3436d2b4f5719f647839b6b45ae7f6
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-01-28 16:49:11 -0400

    [frontend] add ListProyectComponent

---
+++ frontend/src/components/ListProyects/ListProyects.tsx
@@ -0,0 +1,79 @@
+import type { JSX } from "react";
+import { ProyectService, stringProyectStatus, type Proyect } from "../../services/proyect";
+import styles from "./ListProyects.module.css"
+import { getOnlyDate } from "../../utils/times";
+interface propsListProyects {
+    proyects: Proyect[]
+}
+
+interface propsProyectElement {
+    proyect: Proyect
+}
+
+function daysAfter(previousTime: Date): number {
+    const now = new Date();
+
+    const currentDate = getOnlyDate(now);
+    const previousDate = getOnlyDate(previousTime)
+    const diferenceInMs = currentDate.getTime() - previousDate.getTime()
+
+    const milisecondsToDays = 60 * 60 * 1000 * 24;
+    const diference = Math.floor(diferenceInMs / milisecondsToDays)
+
+    return diference
+}
+
+const textCreationDays = (dayCreation: number): string => {
+    if (dayCreation === 0) {
+        return "Creado hoy"
+    }
+    return `Creado hace ${dayCreation} día(s)`
+
+}
+
+
+export const TagStatusProyect = ({ proyect }: propsProyectElement): JSX.Element => {
+    const status = ProyectService.getProyectStatus(proyect)
+
+    const textStatus = stringProyectStatus(status)
+
+    return (
+        <div className={styles.tag + " " + styles[status]}>
+            {textStatus}
+        </div>
+    )
+}
+export const ProyectElement = ({ proyect }: propsProyectElement): JSX.Element => {
+    const dayCreation = daysAfter(proyect.created_at)
+
+    // TODO: create router to onclick to page to proyect
+
+    const onClickGoProyect = () => {
+        alert("hola desde el proyecto con id " + proyect.id)
+    }
+    return (
+        <div className={styles['proyect-elem']} onClick={onClickGoProyect}>
+            <div className={styles.content}>
+                <div className={styles.title}>{proyect.name}</div>
+                <div>
+                    <div>
+                        <p>{textCreationDays(dayCreation)}</p>
+                        <TagStatusProyect proyect={proyect}></TagStatusProyect>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export const ListProyects = ({ proyects }: propsListProyects): JSX.Element => {
+    const proyectsElem = proyects.map(
+        proyect => {
+            return <ProyectElement proyect={proyect} key={proyect.id} />
+        }
+    )
+
+    return <ul className={styles['proyect-list']}>
+        {proyectsElem}
+    </ul>
+}
